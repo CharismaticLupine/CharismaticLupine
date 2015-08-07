76f2df9ab9f020fdad9229d49d724cf0bb6883a2
James Conkling <james.lane.conkling@gmail.com>
2015-08-06 21:06:15 -0700

    hook up request handler for simple GET /physical request

--- server/server-config.js
+++ server/server-config.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Physical = require('./models/physical');
 var app = express();
 
-app.configure(function() {
-  //serve static assets
-  app.use(express.static(__dirname + '/public'));
-  app.use(bodyParser);
-});
+//serve static assets
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/physical', function(req, res){
+  // TODO: specify location as query parameter
+    // Physical.query().fetchAll(...)
+  Physical.fetchAll({withRelated: ['user', 'comment']})
+    .then(function(physicals){
+      res.send(physicals.toJSON());
+    });
 });
 
-app.post('/physical', function(){}/* Add Comment */);
+app.post('/physical', function(req, res){
+});
 
-app.post('/physical/id/comment', function(){} /* Append */);
+app.post('/physical/:id/comment', function(){} /* Append */);
 
-app.post('/physical/id/image', function(){}/* post image */);
+app.post('/physical/:id/image', function(){}/* post image */);
 
 module.exports = app;
