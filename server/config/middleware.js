c4c01fc5665458565f1a2260630675bc456e24f9
Timothy Quach <timothyquachbot@gmail.com>
2015-08-07 15:02:24 -0700

    Implement routing for photo

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -7,6 +7,7 @@ module.exports = function (app, express) {
   // Express 4 allows us to use multiple routers with their own configurations
   var userRouter = express.Router();
   var physicalRouter = express.Router();
+  var photoRouter = express.Router();
 
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
@@ -17,8 +18,10 @@ module.exports = function (app, express) {
   app.use('/users', userRouter); // use user router for all user request
 
   // authentication middleware used to decode token and made available on the request
-  // app.use('/physical', helpers.decode);
+  app.use('/physical', helpers.decode);
   app.use('/physical', physicalRouter); // user link router for link request
+  app.use('/photo', photoRouter);
+
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
 
---
+++ server/photos/photoController.js
@@ -0,0 +1,25 @@
+var Photo = require('./photo.js');
+
+module.exports = {
+  postPhoto: function(req, res, next) {
+    /**
+     * DEBUG: Doesn't work.
+     * Saves photo to filesystem as id.jpg
+     * then saves relationship to database
+     */
+    var user_id = req.body.user_id || 1;//remove default value
+    var physical_id = req.body.physical_id || 1;//remove default value
+    var photoAsset = req.file; // need correct body parser
+
+    var newPhoto = new Photo({ 'physicals_id': physical_id, 'user_id': user_id });
+    //save photo to filesystem
+    newPhoto.save() // placeholder so node doesn't throw errors
+    .then(function(){ return newPhoto.save() })
+    .then(function(photo){
+      res.status(201).send(photo.toJSON());
+    }).catch(function(err){
+      console.log('Error creating new Photo: ', err);
+      res.status(500).send(err);
+    });
+  }
+};
