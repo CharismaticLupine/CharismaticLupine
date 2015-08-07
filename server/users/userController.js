f5c6858ab16473c09030e760c79cbc0ad01ecbb0
Rob Rodrigez <robrod@gmail.com>
2015-08-07 15:16:13 -0700

    Implement sign-in function. Still needs to return a JWT upon successful sign-in

--- server/users/userController.js
+++ server/users/userController.js
@@ -2,7 +2,27 @@ var User = require('./user.js');
 var jwt = require('jwt-simple');
 
 module.exports = {
-  signin: function(req, res, next){},
+  
+  signin: function(req, res, next){
+    var username = req.body.username;
+    var password = req.body.password;
+    new User({user_name: username})
+      .fetch().then(function(user){
+        if(!user){
+          //Not sure how this will react w ionic - intent is to try again!
+          res.redirect('/signin');
+        } else {
+          user.comparePassword(password, function(match){
+            if (match){
+              //Send back JWT
+            } else {
+              res.redirect('/signin');
+            }
+          });
+        }
+      });
+  },
+
   signup: function(req, res, next){},
   checkAuth: function(req, res, next){}
 
