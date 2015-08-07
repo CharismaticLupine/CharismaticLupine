13ae3ad8060f0b9ad9e82fc4b41a30101a4e9594
Rob Rodrigez <robrod@gmail.com>
2015-08-07 14:48:46 -0700

    uncomment user routes in middleware. Create userController and userRoutes files

---
+++ server/users/userController.js
@@ -0,0 +1,9 @@
+var User = require('./user.js');
+var jwt = require('jwt-simple');
+
+module.exports = {
+  signin: function(req, res, next){},
+  signup: function(req, res, next){},
+  checkAuth: function(req, res, next){}
+
+};
