a490463049edbe4f1a342a65aab69b18f34196db
James Conkling <james.lane.conkling@gmail.com>
2015-08-07 14:35:08 -0700

    use recommended way to setting response status

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -12,7 +12,7 @@ module.exports = {
   errorHandler: function (error, req, res, next) {
     // send error message to client
     // message for gracefull error handling on app
-    res.send(500, {error: error.message});
+    res.status(500).send({error: error.message});
   },
 
   decode: function (req, res, next) {
@@ -20,13 +20,13 @@ module.exports = {
     var user;
 
     if (!token) {
-      return res.send(403); // send forbidden if a token is not provided
+      // return res.status(403).end(); // send forbidden if a token is not provided
     }
 
     try {
       // decode token and attach user to the request
       // for use inside our controllers
-      user = jwt.decode(token, 'secret');
+      // user = jwt.decode(token, 'secret');
       req.user = user;
       next();
     } catch(error) {
