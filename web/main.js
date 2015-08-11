15d46c8aeb70f2ac57c0871e36805fbdf5acd1d0
James Conkling <james.lane.conkling@gmail.com>
2015-08-11 15:42:32 -0700

    drop browserify compile

--- web/main.js
+++
@@ -1,5 +0,0 @@
-require('mapbox.js'); // auto-attaches to window.L 
-
-L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXMtbGFuZS1jb25rbGluZyIsImEiOiJ3RHBOc1BZIn0.edCFqVis7qgHPRgdq0WYsA';
-var map = L.mapbox.map('map', 'mapbox.streets', );
-  .setView([40, -174.50], 9);
