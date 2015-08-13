9044f9c455d41c8e542b6eaa68ef40f8a147235d
Rob Rodrigez <robrod@gmail.com>
2015-08-13 11:19:55 -0700

    Implemented Comment Routes

--- server/comments/commentController.js
+++ server/comments/commentController.js
@@ -5,11 +5,11 @@ module.exports = {
 
   getComments: function(req, res, next){
     var physicalId = JSON.parse(req.params['id']);
-   
     Comment.where({physicals_id: physicalId}).fetchAll()
       .then(function(comments){
-        console.log('Success on GET /comments/:id. Returned ' + comments.rows.length + ' results.');
-        res.status(200).send(comments.rows);
+        console.log(comments);
+        console.log('Success on GET /comments/:id. Returned ' + comments.models.length + ' results.');
+        res.status(200).send(comments.models);
         next();
       })
       .catch(function(err){
@@ -17,20 +17,7 @@ module.exports = {
         res.status(500).send(err);
         next();
       });
-    // above code throws unlikely errors: https://github.com/tgriesser/bookshelf/issues/104
-      // so we do the below to manually make the select
-      // use ::geography to cast from geometry to geography type, so that distance measurements are correct
-    knex.raw('SELECT * FROM physicals WHERE ST_DWithin( physicals.geo::geography, ST_SetSRID(ST_Point(' + x + ',' + y + '), 4326)::geography, ' + proximity + ' )')
-      .then(function(physicals){
-        console.log('Success on GET /physical/:location . Returned ' +  physicals.rows.length + ' results.');
-        res.status(200).send(physicals.rows);
-        next();
-      })
-      .catch(function(err){
-        console.log('Error on GET /physical/:location : ', err);
-        res.status(500).send(err);
-        next();
-      });
+   
   },
 
   addComment: function(req, res, next){
@@ -38,31 +25,15 @@ module.exports = {
     var text = req.body.text;
     var physicalId = req.body.phyiscal;
     // define insert query as knex raw SQL
-    knex.raw('INSERT INTO comments (user_id, physicals_id, text) VALUES ( userId, physicalId, text )')
-      .then(function(response){
-        console.log(response);
-        res.status(201).send(response);
-        next();
-      })
-      .catch(function(err){
-        console.log(err);
-        res.status(500).send(err);
-        next();
+    new Comment({user_id: userId, physicals_id: physicalId, text: text})
+      .save()
+      .then(function(model){
+        console.log('Comment successfully added');
+        res.status(201).send(model);;
       });
 
-    // below code throws unlikely errors: https://github.com/tgriesser/bookshelf/issues/104
-      // so we do the above to manually make the insert
-    // var newPhysical = new Physical({ geo: knex.raw('ST_SetSRID( ST_Point(' + x + ',' + y + ') , 4326)') });
-    // var newPhysical = new Physical().set('geo', knex.raw('ST_SetSRID( ST_Point(66,55) , 4326)'));
-    // var newPhysical = new Physical({ geo: req.body.geo });
+   
 
-    // newPhysical.save().then(function(physical){
-    //   res.status(201).send(physical.toJSON());
-    //   next();
-    // }).catch(function(err){
-    //   console.log('Error creating new Physical: ', err);
-    //   res.status(500).send(err);
-    //   next();
-    // });
+    
   }
 };
