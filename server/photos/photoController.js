8200b7862b6c613b9558b433aec2cb5586e14a6f
Rob Rodrigez <robrod@gmail.com>
2015-08-13 12:05:51 -0700

    Implement functionality to get all Photos for a given physical ID

--- server/photos/photoController.js
+++ server/photos/photoController.js
@@ -13,7 +13,7 @@ var savePhotoToFs = function(photoId, buffer) {
   .catch(function(err) {
     console.error('Tried to save photo to disk: ', err);
      return err;
-  })
+  });
 };
 
 var getPhotoFromFs = function(photoId) {
@@ -23,15 +23,12 @@ var getPhotoFromFs = function(photoId) {
 
 module.exports = {
   postPhoto: function(req, res, next) {
-    /**
-     * DEBUG: Doesn't work.
-     * Saves photo to filesystem as id.jpg
-     * then saves relationship to database
-     */
-    var user_id = req.body.user_id;
-    var physical_id = req.body.physical_id;
+   
+    var userId = req.body.user || 1; //remove default value
+    var physicalId = req.body.physical || 1; //remove default value
     var photoAsset =  req.file.buffer;
-    var newPhoto = new Photo({ 'physicals_id': physical_id, 'user_id': user_id });
+
+    var newPhoto = new Photo({ 'physicals_id': physicalId, 'user_id': userId });
     newPhoto.save()
     .then(function(photo){
       var photoId = photo.id;
@@ -43,8 +40,7 @@ module.exports = {
       res.status(500).send(err);
     });
   },
-  getPhoto: function(req, res, next) {
-    console.log('getting photos');
+  getPhotoById: function(req, res, next) {
     var photoId = req.params.id;
     getPhotoFromFs(photoId)
     .then(function(val) {
@@ -53,5 +49,37 @@ module.exports = {
     .catch(function(err) {
       res.status(500).send(err);
     });
+  },
+  getPhotosByPhysical: function(req, res, next) {
+    var physicalId = req.params.id;
+    var result = {photos: []};
+    Photo.where({physical_id: physicalId}).fetchAll()
+      .then(function(photos){
+        
+        photos.models.forEach(function(dbPhoto){
+          getPhotoFromFs(dbPhoto.id)
+            .then(function(photo){
+              result.photos.push(photo);
+            })
+            .catch(function(err){
+              res.status(500).send(err);
+            });
+        });
+        res.status(200).send(result);      
+      })
+      .catch(function(err){
+        req.status(500).send(err);
+      });
+
+    getPhotoFromFs(photoId)
+    .then(function(val) {
+      res.status(200).send(val);
+    })
+    .catch(function(err) {
+      res.status(500).send(err);
+    });
+  },
+  getPhotosByUser: function(req, res, next) {
+    //TODO: Write this function if we require this functionality
   }
 };
--- server/photos/photoRoutes.js
+++ server/photos/photoRoutes.js
@@ -2,6 +2,8 @@ var photoController = require('./photoController.js');
 
 module.exports = function(app) {
   app.post('/', photoController.postPhoto);
-  app.get('/:id', photoController.getPhoto);
+  app.get('/:id', photoController.getPhotoById);
+  app.get('/byPhysical/:id', photoController.getPhotosByPhysical);
+  app.get('/byUser/:id', photoController.getPhotosByUser);
 
 };
